5c6b810eaa165ad956c17454add2b2c3d0140b18
Глазунов Михаил <mishgun.gl@gmail.com>
2024-10-10 22:28:27 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args) {
+        int x [];
+        x = new int [4];
+        x [0] = 0;
+        x [1] = 1;
+        x [2] = 2;
+        x [3] = 3;
+        for (int i = 0; i < 4; i++){
+            System.out.printf(String.valueOf(x [i]));
+        }
+    }
+}
